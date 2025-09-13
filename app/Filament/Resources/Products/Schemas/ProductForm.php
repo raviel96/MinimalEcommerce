193b2bcf648e1df96ffca83aee4445598bf5b0e0
raviel96 <manuelfontaine971@gmail.com>
2025-09-13 16:09:33 -0400

    Modifications of Products table in the database.

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Products\Schemas;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
@@ -31,8 +30,12 @@ class ProductForm
                     ])->columns(2),
 
                     Section::make('Images')->schema([
-                        FileUpload::make('image')
+                        FileUpload::make('images')
+                        ->multiple()
+                        ->disk('public')
                         ->directory('products')
+                        ->maxFiles(5)
+                        ->reorderable()
                     ]),
 
                 ])->columnSpan(2),
@@ -42,6 +45,8 @@ class ProductForm
                         TextInput::make('price')
                         ->required()
                         ->numeric()
+                        ->inputMode('decimal')
+                        ->step(0.01)
                         ->prefix('€')
                         ->minValue(0),
                     ]),
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
         'name',
         'description',
         'price',
-        'image',
+        'images',
         'category_id',
     ];
 
